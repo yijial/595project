4ca7081a465147b62df8a188fbdb9d7c965596d2
Tori Yan <qiaoiyan@cs.washington.edu>
2018-12-01 15:59:20 -0500

    split on business

--- split.py
+++ split.py
@@ -1,35 +1,40 @@
 import sys
+import os
 import math
+from random import randint
 from collections import Counter
 from preprocess import preprocess
 import json
 import numpy as np
 
 vocabulary = []
-input_file = open("../review_.json","r") # txt file input
+business_list = {}
+input_directory = "../json"
 reviews = []
 result = []
-business_list = {}
 p = preprocess()
-data = json.loads(input_file.read())
+
 i=0
-for line in data:
-	text = line["text"]
-	if line["business_id"] not in business_list:
-		business_list[line["business_id"]] = i
-		i+=1
-	result.append([business_list[line["business_id"]], line["useful"], line["review_count"]])
-	
-	tokens = p.preprocess(text)
-	for token in tokens: 
-		if token not in vocabulary:
-			vocabulary.append(token) # word dictionary vocabulary
-	reviews.append(tokens) # array of reviews tokens
-input_file.close()
+for filename in os.listdir(input_directory):
+	if filename.endswith(".json"):
+		input_file = open(input_directory+"/"+filename, "r")
+		data = json.loads(input_file.read())
+		for line in data:
+			text = line["text"]
+			if line["business_id"] not in business_list:
+				business_list[line["business_id"]] = i
+				i+=1
+			result.append([business_list[line["business_id"]], line["useful"], line["review_count"]])
+			
+			tokens = p.preprocess(text)
+			for token in tokens: 
+				if token not in vocabulary:
+					vocabulary.append(token) # word dictionary vocabulary
+			reviews.append(tokens) # array of reviews tokens
+		input_file.close()
 
 bag_words = []
-nums_words = []
-
+business_split = {}
 i=0
 for tokens in reviews:
 	count = Counter(tokens)
@@ -40,8 +45,21 @@ for tokens in reviews:
 	temp = np.array(temp)
 	temp = temp/num_words
 	result[i].extend(temp)
-	nums_words.append(num_words)
+	if result[i][0] not in city_split:
+		business_split[result[i][0]] = []
+	business_split[result[i][0]].append(result[i])
 	i+=1
+train_set = []
+test_set = []
 
-result_np = np.array(result)
-np.save("review_.npy", result_np)
+for business, reviews in business_split.items():
+	print(business)
+	# print(len(reviews))
+	random = randint(0,4)
+	if random == 0:
+		test_set.extend(reviews)
+	else:
+		train_set.extend(reviews)
+
+np.save("train.npy", np.array(train_set))
+np.save("test.npy", np.array(test_set))
