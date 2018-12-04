9c998c495a934900bbf3293543d61313d7b30a7c
YIJIA LIU <yijia987@uw.edu>
2018-12-03 20:16:51 -0500

    training nn on batch

--- model_nn.py
+++ model_nn.py
@@ -3,66 +3,84 @@ import torch
 import torch.optim
 import torch.nn as nn
 import sys
+import os 
 from sklearn.utils import shuffle
+from scipy import sparse
+
+# directory = "toronto_npz/"
 
 
-def main(train, test):
-	data = np.load(train)
-	# print(data.shape)
-	shuffle(data)
-	# business_id, useful, review_count, bag_of_words
-	label = torch.from_numpy(data[:,1].reshape(-1,1)).float()
-	features = torch.from_numpy(data[:, 2:]).float()
-	# print(label.shape)
-	# print(features.shape)
-
+def main(dir):
 	# dimensions of the nn
-	n_in, n_h1, n_h2, n_out = features.shape[1], 100, 100, 1
+	n_in, n_h1, n_h2, n_out = 42288, 100, 100, 1
 	# condition of converge
 	thres = 0.001
 	model = nn.Sequential(nn.Linear(n_in, n_h1),
-			nn.Tanh(),
-			nn.Linear(n_h1, n_h2),
-			nn.Sigmoid(),
-			nn.Linear(n_h2, n_out),
-			# nn.Sigmoid()
-			)
+		nn.Tanh(),
+		nn.Linear(n_h1, n_h2),
+		nn.Sigmoid(),
+		nn.Linear(n_h2, n_out),
+		# nn.Sigmoid()
+		)
 
 	criterion = torch.nn.MSELoss()
 	optimizer = torch.optim.SGD(model.parameters(), lr=0.01)
 
 	prev_loss = 0
 
-	for epoch in range(10000):
-		y_pred = model(features)
+	# train the model on training data
+	for filename in os.listdir(dir):
+		if filename.startswith("train"):
+			print("training on: " + filename)
+			data = sparse.load_npz(dir + '/' + filename).toarray()
+			# print(data.shape)
+			shuffle(data)
+			# business_id, useful, review_count, bag_of_words
+			label = torch.from_numpy(data[:,1].reshape(-1,1)).float()
+			features = torch.from_numpy(data[:, 2:]).float()
+			# print(label.shape)
+			# print(features.shape)
+			for epoch in range(100):
+				y_pred = model(features)
 
-		loss = criterion(y_pred, label)
-		if(epoch % 100 == 0):
-			print('epoch: ', epoch,' loss: ', loss.item())
-			# converges, break
-		
-		if abs(prev_loss - loss.item()) <= thres:
-			break
-		prev_loss = loss.item()
+				loss = criterion(y_pred, label)
+				if(epoch % 100 == 0):
+					print('epoch: ', epoch,' loss: ', loss.item())
+					# converges, break
+				
+				if abs(prev_loss - loss.item()) <= thres:
+					break
+				prev_loss = loss.item()
 
-		optimizer.zero_grad()
-		loss.backward()
-		optimizer.step()
+				optimizer.zero_grad()
+				loss.backward()
+				optimizer.step()
 
-	testdata = np.load(test)
-	test_label = torch.from_numpy(testdata[:, 1].reshape(-1,1)).float()
-	test_feature = torch.from_numpy(testdata[:, 2:]).float()
-	label_pred = model(test_feature)
-	loss = criterion(label_pred, test_label)
-	label_pred = label_pred.detach().numpy().reshape(-1,1)
-	
+
+	standard = np.empty((0,1))
+	pred = np.empty((0,1))
+	bid = np.empty((0,1))
+	# predict labels for test data
+	for filename in os.listdir(dir):
+		if filename.startswith("test"):
+			print("generating label on: " + filename)
+			data = sparse.load_npz(dir + '/' + filename).toarray()
+			label = torch.from_numpy(data[:,1].reshape(-1,1)).float()
+			features = torch.from_numpy(data[:, 2:]).float()
+			label_pred = model(features)
+			# loss = criterion(label_pred, label)
+			label_pred = label_pred.detach().numpy().reshape(-1,1)
+			bid = np.concatenate((bid, data[:,0].reshape(-1,1)),axis=0)
+			standard = np.concatenate((standard, label.numpy().reshape(-1,1)),axis=0)
+			pred = np.concatenate((pred, label_pred), axis=0)
+
 	# format: golden label, predicted, business_id
-	output = np.concatenate((testdata[:, 1].reshape(-1,1), label_pred), axis=1)
-	output = np.concatenate((output, testdata[:, 0].reshape(-1,1)), axis=1)
+	output = np.concatenate((bid, standard, pred), axis=1)
+	csr_matrix = sparse.csr_matrix(output)
 	print(output.shape)
-	print('test loss: ' + str(loss.item()))
-	np.save("nn_output.npy", output)
+	sparse.save_npz("nn_output.npz", csr_matrix)
+
 
 if __name__ == "__main__":
-	main(sys.argv[1], sys.argv[2])
+	main(sys.argv[1])
 
